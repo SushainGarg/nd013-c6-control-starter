ccdab1f50afc60fb6ecf0875d2374d42971f4d17
SushainGarg <78366790+SushainGarg@users.noreply.github.com>
2025-03-02 07:52:14 +0000

    Update pid_controller.cpp

--- project/pid_controller/pid_controller.cpp
+++ project/pid_controller/pid_controller.cpp
@@ -19,6 +19,17 @@ void PID::Init(double Kpi, double Kii, double Kdi, double output_lim_maxi, doubl
    /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    **/
+   _Kpi = Kpi; 
+   _Kii = Kii;
+   _Kdi = Kdi;
+   _output_lim_maxi = output_lim_maxi;
+   _output_lim_mini = output_lim_mini;
+  //  _dKpi = 1; 
+  //  _dKii = 1;
+  //  _dKdi = 1;
+  //  _dKpi = 1; 
+  //  _dKii = 1;
+  //  _dKdi = 1;
 }
 
 
@@ -26,6 +37,17 @@ void PID::UpdateError(double cte) {
    /**
    * TODO: Update PID errors based on cte.
    **/
+  if(_delta_time != 0){
+    _diff_cte = (cte - _cte)/_delta_time;
+    
+  
+  }
+  else {
+    _diff_cte = 0;
+  }
+  _cte = cte;
+  _icte += _cte;
+
 }
 
 double PID::TotalError() {
@@ -33,12 +55,21 @@ double PID::TotalError() {
    * TODO: Calculate and return the total error
     * The code should return a value in the interval [output_lim_mini, output_lim_maxi]
    */
-    double control;
+
+    double control = (-_Kpi * _cte) + (_Kdi * _diff_cte) + (_Kii * _icte);
+
+    if(control > _output_lim_maxi){
+      control = _output_lim_maxi;
+    }
+    else if(control < _output_lim_mini){
+      control = _output_lim_mini;
+    }
     return control;
 }
 
-double PID::UpdateDeltaTime(double new_delta_time) {
+void PID::UpdateDeltaTime(double new_delta_time) {
    /**
    * TODO: Update the delta time with new value
    */
+  _delta_time = new_delta_time;
 }
