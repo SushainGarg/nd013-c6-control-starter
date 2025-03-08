45f8664b65296de704472f065e06d2a542ccb8c0
SushainGarg <78366790+SushainGarg@users.noreply.github.com>
2025-03-08 18:51:17 +0000

    Update pid_controller.cpp

--- project/pid_controller/pid_controller.cpp
+++ project/pid_controller/pid_controller.cpp
@@ -19,53 +19,52 @@ void PID::Init(double Kpi, double Kii, double Kdi, double output_lim_maxi, doubl
    /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    **/
-   _Kpi = Kpi; 
-   _Kii = Kii;
-   _Kdi = Kdi;
-   _output_lim_maxi = output_lim_maxi;
-   _output_lim_mini = output_lim_mini;
-   _icte = 0.0;
-   _first_time = true;
-   _prev_cte = 0.0;
-   _prev_diff_cte = 0.0; 
-  //  _dKpi = 1; 
-  //  _dKii = 1;
-  //  _dKdi = 1;
-  //  _dKpi = 1; 
-  //  _dKii = 1;
-  //  _dKdi = 1;
+  this-> Kpi = Kpi;
+  this-> Kdi = Kdi;
+  this-> Kii = Kii;
+  this-> output_lim_maxi = output_lim_maxi;
+  this-> output_lim_mini = output_lim_mini;
+  PError = 0;
+  IError = 0;
+  DError = 0;
+  //this-> new_delta_time = 0;
+  preverr = 0;
 }
 
 
 void PID::UpdateError(double cte) {
-   _cte = cte;
-   _icte += _cte * _delta_time;
-   // using integral windup here, reffered from multiple sources
-  //  if(_output_lim_maxi > _cte){
-  //     i_max = _output_lim_maxi - _cte
-  //  }
-  //  else{
-  //     i_max = 0.0
-  //  }
-  // if (_output_lim_min < _cte){
-  //    i_max = _output_lim_min - _cte;
-  //  }
-  // else{
-  //     i_min = 0.0;
-  //  }
-  //  if(i_cte > i_max){
-  //     i_cte = i_max
-  //  }
-  //  else if(i_cte < i_max){
-  //     i_cte = i_min
-  //  }
-   if (_delta_time > 0) {
-         _diff_cte = (cte - _prev_cte) / _delta_time;
-   } else {
-       _diff_cte = _prev_diff_cte;
-   }
-   _prev_cte = _cte;
-   _prev_diff_cte = _diff_cte;
+   /**
+   * TODO: Update PID errors based on cte.
+   **/
+  IError += cte* this ->new_delta_time;
+  PError = cte;
+  
+  /*The next few lines of code include the integral windup for tuning PID gains. This is done by preventing the integral term from accumulating above or below pre-determined bounds as mentioned in https://en.wikipedia.org/wiki/Integral_windup#cite_note-4.
+  The codes are based on http://brettbeauregard.com/blog/2011/04/improving-the-beginner%e2%80%99s-pid-reset-windup/
+  and https://github.com/BanaanKiamanesh/Anti-Windup-PID-Controller/blob/main/PID.cpp
+  */
+  if (output_lim_maxi > PError)
+      lim_max_integ = output_lim_maxi - PError;  // Limit maximum integral bounds
+  else
+      lim_max_integ = 0.0;
+
+  if (output_lim_mini < PError)
+     lim_max_integ = output_lim_mini - PError; // Limit minimium integral bounds
+  else
+        lim_min_integ = 0.0;
+
+    // Limit Integrator gain
+     if (IError > lim_max_integ)
+        IError = lim_max_integ;
+
+     else if (IError < lim_min_integ)
+         IError = lim_min_integ;
+  
+  if (this ->new_delta_time>0)
+  {
+    DError = (cte-preverr)/this->new_delta_time ;
+  }
+  preverr = cte;
 }
 
 double PID::TotalError() {
@@ -73,21 +72,23 @@ double PID::TotalError() {
    * TODO: Calculate and return the total error
     * The code should return a value in the interval [output_lim_mini, output_lim_maxi]
    */
-
-    double control = (-_Kpi * _cte) - (_Kdi * _diff_cte) - (_Kii * _icte);
-
-    if(control > _output_lim_maxi){
-      control = _output_lim_maxi;
+    double control;
+    control = Kpi*PError + Kii*IError +Kdi*DError;
+    if (control < output_lim_mini)
+    {
+     control = output_lim_mini;
     }
-    else if(control < _output_lim_mini){
-      control = _output_lim_mini;
+    else if (control > output_lim_maxi)
+    {
+      control = output_lim_maxi;
     }
     return control;
 }
 
-void PID::UpdateDeltaTime(double new_delta_time) {
+double PID::UpdateDeltaTime(double new_delta_time) {
    /**
    * TODO: Update the delta time with new value
-   */
-  _delta_time = new_delta_time;
+   */  
+   this ->new_delta_time = new_delta_time;
+   return new_delta_time;
 }
